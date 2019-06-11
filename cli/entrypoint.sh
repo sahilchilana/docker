a149d719e1ca40743eac27d5ebc906d6c9271596
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-11 15:42:39 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -2,6 +2,8 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
 docker pull amazon/aws-stepfunctions-local
 docker run -p 8083:8083 amazon/aws-stepfunctions-local
 aws stepfunctions --endpoint http://localhost:8083 create-state-machine --definition '{
