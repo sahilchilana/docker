cf2100b460c08669ac7d8e61b1c123f7d90cd3c8
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-11 16:42:22 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -l
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
-export AWS_DEFAULT_REGION=us-east-1
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_DEFAULT_REGION
 docker pull amazon/aws-stepfunctions-local
 docker run -p 8083:8083 amazon/aws-stepfunctions-local
       
