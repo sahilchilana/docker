b21951e8af0967c35523bfb91405b2bfef8b52db
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-11 15:43:42 +0530

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -5,27 +5,5 @@ export AWS_DEFAULT_REGION=us-east-1
 echo $AWS_ACCESS_KEY_ID
 echo $AWS_SECRET_ACCESS_KEY
 docker pull amazon/aws-stepfunctions-local
-docker run -p 8083:8083 amazon/aws-stepfunctions-local
-aws stepfunctions --endpoint http://localhost:8083 create-state-machine --definition '{
-              "Comment": "Add two numbers and then subtact the result of add with another number",
-              "StartAt": "AddNumbers",
-              "States": {
-                "AddNumbers": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-AddFunction-1KWO14YS9KT41",
-                  "Parameters":{
-                      "number1":10,
-                      "number2":20,
-                      "number3":50
-                  },
-                  "Next": "SubtractNumbers"
-                },
-                  "SubtractNumbers": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:us-east-1:670868576168:function:githubactiontesting-SubtractFunction-G5OHM6HZF29F",
-                  "End": true
-                } 
-                }
-              }
-            }' --name "statemachine" --role-arn "arn:aws:iam::670868576168:role/githubactiontesting-AddFunctionRole-828QDZ3VB97V"
+
       
